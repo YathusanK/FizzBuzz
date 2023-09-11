ca9d0c3d802733a973baa3e52cfd76417504f41d
Yathusan Koneswararajah <yathusank630@gmail.com>
2023-09-11 15:52:33 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -30,5 +30,30 @@ class FizzBuzz {
 
             }
         }
+        int max = 0;
+        while (max < 100) {
+
+            max = doFizzBuzz(max);
+        }
+    }
+
+    private static int doFizzBuzz(int max) {
+        boolean divisibleBy3 = max % 3 == 0;
+        boolean divisibleBy5 = max % 5 == 0;
+
+        if (divisibleBy3 && divisibleBy5){
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+            System.out.println("Buzz");
+
+        }else {
+            System.out.println(max);
+        }
+        max++;
+        return max;
     }
 }
